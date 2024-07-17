d12be963d3c7a72caa499f586b5e9534bbaf5ee5
Akashyadav1997 <akashyd11@gmail.com>
2024-07-17 23:06:12 +0530

    checking cors error

--- server/app.js
+++ server/app.js
@@ -8,10 +8,12 @@ const app = express();
 dotenv.config({
 	path: "./.env",
 });
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
 const uri = process.env.MONGO_URI;
 // body parser
 app.use(express.json());
+console.log("below is teh server url");
+console.log(process.env.CLIENT_URL);
 app.use(
 	cors({
 		origin: process.env.CLIENT_URL,
