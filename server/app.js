46df2b62168bdb54f35f3f0c225db076ef4a044f
Akashyadav1997 <akashyd11@gmail.com>
2024-07-18 07:00:12 +0530

    cors issue solving atttenpt1

--- server/app.js
+++ server/app.js
@@ -4,33 +4,39 @@ import applicant from "./routes/applicant.js";
 import { connectDB } from "./utils/feature.js";
 import { errorMiddleware } from "./middleware/error.js";
 import cors from "cors";
+
 const app = express();
 dotenv.config({
-	path: "./.env",
+  path: "./.env",
 });
+
 const port = process.env.PORT || 8000;
 const uri = process.env.MONGO_URI;
-// body parser
+
+// Body parser
 app.use(express.json());
+
+// CORS configuration
 app.use(
-	cors({
-		origin: process.env.CLIENT_URL,
-		methods: ["GET", "POST", "PUT", "DELETE"],
-		allowedHeaders: ["my-custom-header"],
-		credentials: true,
-	})
+  cors({
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
 );
+
 app.use(applicant);
 
-//deafult route
-app.use("/", (req, res, next) => {
-	res.send("Akash Yadav Portfolio Server Side Default Route");
+// Default route
+app.use("/", (req, res) => {
+  res.send("Akash Yadav Portfolio Server Side Default Route");
 });
 
-//handle all errors
+// Handle all errors
 app.use(errorMiddleware);
 
 app.listen(port, () => {
-	connectDB(uri);
-	console.log("Server is listening on", port);
+  connectDB(uri);
+  console.log("Server is listening on", port);
 });
